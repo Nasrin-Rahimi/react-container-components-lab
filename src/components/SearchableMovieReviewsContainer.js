97c65c88aa529dcd97777a6078bf237e2b217a50
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-01 05:44:39 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -5,7 +5,7 @@ import MovieReviews from './MovieReviews'
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
-
+//This component is not complete. You should add search form!
 class SearchableMovieReviewsContainer extends React.Component{
     constructor(){
         super()
